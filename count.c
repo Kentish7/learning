d30a0905a937fef0d31c5d428996743f52e77271
Luis Rodrigues <luishenriquesc2020@gmail.com>
2023-01-01 21:19:07 -0300

    c program

---
+++ count.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+
+    printf("It will print the numbers 1 to 100\n");
+
+    for(int i = 0; i <= 100; i++){
+      printf("%d ", i);
+    }
+    return 0;
+}
+ 
